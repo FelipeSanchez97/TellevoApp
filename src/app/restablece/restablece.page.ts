9a8d644b5c76d479c7a798d8a0141e2246567ad9
PipeSB <fel.sanchezr@duocuc.cl>
2023-08-30 20:56:08 -0400

    agrego las paginas de inicio y restablecer pass

---
+++ src/app/restablece/restablece.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restablece',
+  templateUrl: './restablece.page.html',
+  styleUrls: ['./restablece.page.scss'],
+})
+export class RestablecePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
