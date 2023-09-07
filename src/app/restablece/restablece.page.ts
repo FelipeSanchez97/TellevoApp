b6b46be5a99ebbd33a1cb4ad31e4a3cfe2bd9d84
PipeSB <fel.sanchezr@duocuc.cl>
2023-09-06 22:20:20 -0300

    ag msg de error en rest y agrego el saludo al usr

--- src/app/restablece/restablece.page.ts
+++ src/app/restablece/restablece.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 
 
@@ -16,51 +18,34 @@ export class RestablecePage implements OnInit {
   passwordsMatch: boolean = true;
   
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private alertController: AlertController, private router: Router) { }
 
   ngOnInit() {
   }
 
   resetPassword() {
     // Validar que los campos no estén vacíos y que la nueva contraseña sea igual a la confirmación.
-    if (!this.username || !this.currentPassword || !this.newPassword) {
-      console.error('Por favor, complete todos los campos.');
+    if (!this.username || !this.currentPassword || !this.newPassword || !this.confirmPassword) {
+      this.presentAlert('Datos incompletos', 'Por favor, complete todos los campos.');
       return;
     }
     if (this.newPassword !== this.confirmPassword) {
-      this.passwordsMatch = false; // Establece la variable de bandera en falso
+      this.passwordsMatch = false;
+      this.presentAlert('Contraseñas no coinciden', 'Las contraseñas no coinciden. Verifica nuevamente.'); // Establece la variable de bandera en falso
       return;
     }
-
-    if (this.newPassword !== this.confirmPassword) {
-      console.error('Las contraseñas no coinciden. Verifica nuevamente.');
-      return;
-    }
-    if (this.newPassword !== this.confirmPassword) {
-      this.passwordsMatch = false; // Establece la variable de bandera en falso
-      return;
-    }
+    this.router.navigate(['/home']);
+    
   }
+  async presentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['Aceptar']
+    });
+  
+    await alert.present();
+}
 }
 
-    /* // Aquí debes enviar una solicitud al backend para restablecer la contraseña.
-    // Esto puede implicar el uso de una API o un servicio web.
-
-    const resetData = {
-      username: this.username,
-      newPassword: this.newPassword,
-    };
-
-    this.http.post('URL_DEL_BACKEND/reset-password', resetData).subscribe(
-      (response) => {
-        console.log('Solicitud de restablecimiento de contraseña enviada con éxito.');
-        // Puedes redirigir al usuario a una página de confirmación o realizar otra acción necesaria.
-      },
-      (error) => {
-        console.error('Error al enviar la solicitud de restablecimiento de contraseña:', error);
-        // Maneja errores de acuerdo a tus necesidades.
-      }
-    );
-  }
-} */
-
+    
